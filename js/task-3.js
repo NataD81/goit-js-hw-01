0278d0cd5c0172c8238f3eaae4c831311a8698c8
AlexeyDvorianchuk <153747024+NataD81@users.noreply.github.com>
2024-04-08 16:22:01 +0300

    go

--- js/task-3.js
+++ js/task-3.js
@@ -1,25 +1,39 @@
-function filterArray(numbers, value) {
-    const filterArrayNew = [];
-    if (numbers.length === 0) {
-        return filterArrayNew;
-    }
-    for (let i = 0; i < numbers.length; i += 1) {
-        if (numbers[i] > value) {
-            filterArrayNew.push(numbers[i]);
-        }
-    }
-    return filterArrayNew;
-}
-
-
-console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
-console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
-console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
-console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
-console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
-
-
-
+const profile = {
+    username: "Jacob",
+    playTime: 300,
+    // Метод changeUsername(newName) повинен приймати рядок (нове ім’я)
+    //  в параметр newName та змінювати значення властивості username на нове. 
+    //  Нічого не повертає.
+    changeUsername(newName) {
+      profile.username = newName;
+    },
+    // // Метод updatePlayTime(hours) повинен приймати число (кількість годин) 
+    // у параметр hours та збільшити на нього значення властивості playTime. 
+    // Нічого не повертає.
+    updatePlayTime(hours){
+      if (profile.playTime === this.playTime){
+        profile.playTime += hours;
+      }
+    },
+    // Метод getInfo() має повертати рядок формату <Username>
+    //  has <amount> active hours!, де <Username> — це ім’я профілю, а <amount>
+    //   — кількість ігрових годин.
+    getInfo(){
+      return `${profile.username} has ${profile.playTime} active hours!`;
+    },
+  }
+  
+  
+    
+  
+  
+  console.log(profile.getInfo()); // "Jacob has 300 active hours!"
+  
+  profile.changeUsername("Marco");
+  console.log(profile.getInfo()); // "Marco has 300 active hours!"
+  
+  profile.updatePlayTime(20);
+  console.log(profile.getInfo()); // "Marco has 320 active hours!"
 
 
 
