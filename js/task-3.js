d5a040d713075fe8e99061a4282e279ab9744df3
AlexeyDvorianchuk <153747024+NataD81@users.noreply.github.com>
2024-04-10 22:23:45 +0300

    task-3

--- js/task-3.js
+++ js/task-3.js
@@ -4,22 +4,21 @@ const profile = {
     // Метод changeUsername(newName) повинен приймати рядок (нове ім’я)
     //  в параметр newName та змінювати значення властивості username на нове. 
     //  Нічого не повертає.
-    changeUsername(newName) {
-      profile.username = newName;
-    },
+    changeUsername (newName) {
+      this.username = newName;
+    },   
+    
     // // Метод updatePlayTime(hours) повинен приймати число (кількість годин) 
     // у параметр hours та збільшити на нього значення властивості playTime. 
     // Нічого не повертає.
     updatePlayTime(hours){
-      if (profile.playTime === this.playTime){
-        profile.playTime += hours;
-      }
+      this.playTime += hours
     },
     // Метод getInfo() має повертати рядок формату <Username>
     //  has <amount> active hours!, де <Username> — це ім’я профілю, а <amount>
     //   — кількість ігрових годин.
     getInfo(){
-      return `${profile.username} has ${profile.playTime} active hours!`;
+      return `${this.username} has ${this.playTime} active hours!`;
     },
   }
   
