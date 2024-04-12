ec6802f85a3d975b2b77bdc315535cabd883cb64
AlexeyDvorianchuk <153747024+NataD81@users.noreply.github.com>
2024-04-12 18:29:54 +0300

    js-hw-05

--- js/task-3.js
+++ js/task-3.js
@@ -1,46 +1,80 @@
-const profile = {
-    username: "Jacob",
-    playTime: 300,
-    // Метод changeUsername(newName) повинен приймати рядок (нове ім’я)
-    //  в параметр newName та змінювати значення властивості username на нове. 
-    //  Нічого не повертає.
-    changeUsername (newName) {
-      this.username = newName;
-    },   
-    
-    // // Метод updatePlayTime(hours) повинен приймати число (кількість годин) 
-    // у параметр hours та збільшити на нього значення властивості playTime. 
-    // Нічого не повертає.
-    updatePlayTime(hours){
-      this.playTime += hours
+const sortByDescendingFriendCount = (users) => {
+  return users.toSorted((a, b) => b.friends.length - a.friends.length);  
+};
+
+console.log(
+  sortByDescendingFriendCount([
+    {
+      name: "Moore Hensley",
+      friends: ["Sharron Pace"],
+      gender: "male"
     },
-    // Метод getInfo() має повертати рядок формату <Username>
-    //  has <amount> active hours!, де <Username> — це ім’я профілю, а <amount>
-    //   — кількість ігрових годин.
-    getInfo(){
-      return `${this.username} has ${this.playTime} active hours!`;
+    {
+      name: "Sharlene Bush",
+      friends: ["Briana Decker", "Sharron Pace"],
+      gender: "female"
     },
-  }
-  
-  
-    
-  
-  
-  console.log(profile.getInfo()); // "Jacob has 300 active hours!"
-  
-  profile.changeUsername("Marco");
-  console.log(profile.getInfo()); // "Marco has 300 active hours!"
-  
-  profile.updatePlayTime(20);
-  console.log(profile.getInfo()); // "Marco has 320 active hours!"
-
-
-
-
-
-
-
-
-
-
-
+    {
+      name: "Ross Vazquez",
+      friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
+      gender: "male"
+    },
+    {
+      name: "Elma Head",
+      friends: ["Goldie Gentry", "Aisha Tran"],
+      gender: "female"
+    },
+    {
+      name: "Carey Barr",
+      friends: ["Jordan Sampson", "Eddie Strong"],
+      gender: "male"
+    },
+    {
+      name: "Blackburn Dotson",
+      friends: ["Jacklyn Lucas", "Linda Chapman"],
+      gender: "male"
+    },
+    {
+      name: "Sheree Anthony",
+      friends: ["Goldie Gentry", "Briana Decker"],
+      gender: "female"
+    }
+  ])
+ );
+//  [
+//  {
+//     name: "Ross Vazquez",
+//     friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
+//     gender: "male"
+//   },
+//   {
+//     name: "Sharlene Bush",
+//     friends: ["Briana Decker", "Sharron Pace"],
+//     gender: "female"
+//   },
+//   {
+//     name: "Elma Head",
+//     friends: ["Goldie Gentry", "Aisha Tran"],
+//     gender: "female"
+//   },
+//   {
+//     name: "Carey Barr",
+//     friends: ["Jordan Sampson", "Eddie Strong"],
+//     gender: "male"
+//   },
+//   {
+//     name: "Blackburn Dotson",
+//     friends: ["Jacklyn Lucas", "Linda Chapman"],
+//     gender: "male"
+//   },
+//   {
+//     name: "Sheree Anthony",
+//     friends: ["Goldie Gentry", "Briana Decker"],
+//     gender: "female"
+//   },
+//   {
+//     name: "Moore Hensley",
+//     friends: ["Sharron Pace"],
+//     gender: "male"
+//   }
+// ]
